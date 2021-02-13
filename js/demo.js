97e277c4cc5db34aaf673a02e1019b10c2d70740
Jithin Scaria <admin@jjscaria.com>
2021-02-13 11:42:41 -0600

    updating to personal info

--- js/demo.js
+++ js/demo.js
@@ -6,8 +6,8 @@
 // This can be used to set the Particles Effects. Check README for more details!
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
+    dotColor: '#800080',
+    lineColor: '#800080'
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
